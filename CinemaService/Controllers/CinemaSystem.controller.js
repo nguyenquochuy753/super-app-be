661215314a38799b7532671041eb2b56c9211833
nguyenquochuy753 <92136604+nguyenquochuy753@users.noreply.github.com>
2024-03-28 19:41:45 +0700

    feature api[cinemaSystem]: get, update, delete

--- CinemaService/Controllers/CinemaSystem.controller.js
+++ CinemaService/Controllers/CinemaSystem.controller.js
@@ -19,6 +19,45 @@ const cinemaSystemController = {
         } catch (error) {
             res.status(500).json(error);
         }
+    },
+
+    updateCinemaSystem: async(req,res)=>{
+        try {
+            const {_id} = req.params;
+            await cinemaSystemModel.findByIdAndUpdate(_id,req.body,{new: true});
+            res.status(200).json("Updated successfully");
+        } catch (error) {
+            res.status(400).json(error);
+        }
+    },
+
+    getAllCinemaSystem: async(req,res)=>{
+        try {
+            const cinemaSystems = await cinemaSystemModel.find({});
+            res.status(200).json(cinemaSystems);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+    getCinemaSystemByID: async(req,res)=>{
+        try {
+            const {_id} = req.params;
+            const cinemaSystem = await cinemaSystemModel.findById(_id);
+            res.status(200).json(cinemaSystem);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+    deleteCinemaSystemById: async(req,res)=>{
+        try {
+            const {_id} = req.params;
+            await cinemaSystemModel.findByIdAndDelete(_id);
+            res.status(200).json("Deleted successfully");
+        } catch (error) {
+            res.status(500).json(error);
+        }
     }
 
 }
--- CinemaService/Routes/CinemaSystem.route.js
+++ CinemaService/Routes/CinemaSystem.route.js
@@ -2,5 +2,9 @@ const cinemaSystemController = require('../Controllers/CinemaSystem.controller')
 const router = require('express').Router();
 
 router.post('/',cinemaSystemController.addCinemaSystem);
+router.patch('/:_id',cinemaSystemController.updateCinemaSystem);
+router.get('/',cinemaSystemController.getAllCinemaSystem);
+router.get('/:_id',cinemaSystemController.getCinemaSystemByID);
+router.delete('/:_id',cinemaSystemController.deleteCinemaSystemById);
 
 module.exports = router;
