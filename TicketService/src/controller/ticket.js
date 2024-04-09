e807f18d26bb4132bf4be95f4a98bdc72ca16735
VU QUOC HUNG <20521378@gm.uit.edu.vn>
2024-04-09 10:38:54 +0700

    Save

--- TicketService/src/controller/ticket.js
+++ TicketService/src/controller/ticket.js
@@ -16,3 +16,52 @@ exports.createTicket = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+
+exports.updateTicket = async (req, res) => {
+  try {
+    const { _id } = req.params;
+    await Ticket.findByIdAndUpdate(_id, req.body, { new: true });
+    res.status(200).json("Updated successfully");
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+exports.getAllTicket = async (req, res) => {
+  try {
+    const tickets = await Ticket.find({});
+    res.status(200).json(tickets);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+exports.getTicketById = async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const ticket = await Ticket.findById(_id);
+    res.status(200).json(ticket);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+exports.deleteTicketById = async (req, res) => {
+  try {
+    const { _id } = req.params;
+    await Ticket.findByIdAndDelete(_id);
+    res.status(200).json("Deleted successfully");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+exports.getTicketByUser = async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const tickets = await Ticket.find({ user: _id });
+    res.status(200).json(tickets);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
--- TicketService/src/routes/ticket.js
+++ TicketService/src/routes/ticket.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const { createTicket } = require("../controller/ticket");
+const { createTicket, updateTicket, getAllTicket, getTicketById, deleteTicketById } = require("../controller/ticket");
 const router = express.Router();
 
 router.post("/ticket/create", createTicket);
+router.patch("/:_id", updateTicket);
+router.get("/", getAllTicket);
+router.get("/:_id", getTicketById);
+router.delete("/:_id", deleteTicketById);
+
 
 module.exports = router;
